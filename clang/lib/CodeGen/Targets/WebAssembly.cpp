511122b9793e1075547ef83b8ad5a0b841123f68
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-06-24 12:25:03 +0200

    Only generate thunks for constant function pointers

--- clang/lib/CodeGen/Targets/WebAssembly.cpp
+++ clang/lib/CodeGen/Targets/WebAssembly.cpp
@@ -97,6 +97,11 @@ public:
   llvm::Function *getOrCreateWasmFunctionPointerThunk(
       CodeGenFunction &CGF, llvm::Value *OriginalFnPtr, QualType SrcType,
       QualType DstType) const override {
+    // Only generate thunks for constant function pointers.
+    if (!isa<llvm::Constant>(OriginalFnPtr)) {
+      return nullptr;
+    }
+
     llvm::Module &M = CGF.CGM.getModule();
 
     // Get the signatures.
