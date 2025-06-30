5d6570f80fcbad45e85dba41bf37ed1b617f7c05
Jorge Zapata <jorgeluis.zapata@gmail.com>
2025-06-30 13:48:22 +0200

    fixup! fixup! [wasm] Support different signature function pointers

--- clang/lib/CodeGen/CGExprScalar.cpp
+++ clang/lib/CodeGen/CGExprScalar.cpp
@@ -2475,19 +2475,6 @@ Value *ScalarExprEmitter::VisitCastExpr(CastExpr *CE) {
       }
     }
 
-    // For WebAssembly target we need to create thunk functions
-    // to properly handle function pointers with a different signature.
-    // Due to opaque pointers, this can not be handled in LLVM
-    // (WebAssemblyFixFunctionBitcast) anymore
-    if (CGF.CGM.getTriple().isWasm()) {
-      QualType SourceTy = E->getType();
-      if (SourceTy->isFunctionPointerType() && DestTy->isFunctionPointerType()) {
-        llvm::Function *Thunk = CGF.CGM.getTargetCodeGenInfo().getOrCreateWasmFunctionPointerThunk(
-              CGF, Src, SourceTy, DestTy);
-        if (Thunk) return Thunk;
-      }
-    }
-
     if (CGF.CGM.getCodeGenOpts().StrictVTablePointers) {
       const QualType SrcType = E->getType();
 
--- clang/lib/CodeGen/TargetInfo.h
+++ clang/lib/CodeGen/TargetInfo.h
@@ -421,7 +421,9 @@ public:
   /// Return the WebAssembly funcref reference type.
   virtual llvm::Type *getWasmFuncrefReferenceType() const { return nullptr; }
 
-  virtual llvm::Function * getOrCreateWasmFunctionPointerThunk(CodeGenFunction &CGF,
+  virtual const DeclRefExpr * getWasmFunctionDeclRefExpr(const Expr *E, ASTContext &Ctx) const { return nullptr; }
+
+  virtual llvm::Function * getOrCreateWasmFunctionPointerThunk(CodeGenModule &CGM,
     llvm::Value *OriginalFnPtr, QualType SrcType, QualType DstType) const { return nullptr; }
 
   /// Emit the device-side copy of the builtin surface type.
